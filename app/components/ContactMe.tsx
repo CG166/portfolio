cab7ddd85cbc36961a4b2a8bff7c43132dbd3a8f
Cindy Garcia <cinderg166@gmail.com>
2026-09-20 21:16:22 -0700

    Working on text + format adaptability

--- app/components/ContactMe.tsx
+++ app/components/ContactMe.tsx
@@ -22,8 +22,8 @@ export default function ContactMe() {
             <h1 className="absolute top-0  left-1/2 -translate-x-1/2 -translate-y-1/2 bg-black px-3 text-white font-orbitron text-[clamp(1rem,5cqw,12rem)]">
                 Channels
             </h1>
-                <div className="w-[80%] h-[80%] font-orbitron p-10 flex justify-between">
-                    <div>
+                <div className="w-[80%] h-[80%] font-orbitron p-10 flex justify-between gap-10 border-2 border-amber-300">
+                    <div className="w-1/2 min-w-0">
                         <h1 className="text-4xl pb-10 text-white text-[clamp(1.25rem,2.5vw,2.5rem)]">[ ESTABLISH UPLINK ]</h1>
                         <div className="text-3xl pl-10 text-[clamp(1rem,2vw,1.875rem)]">
                             <a href="mailto:cgarcia.software.tech@gmail.com" className="block pb-4 hover:text-cyan-400 hover:text-[2rem] cursor-pointer text-white">
@@ -40,7 +40,7 @@ export default function ContactMe() {
                         </div>
                     </div>
 
-                    <div>
+                    <div className="w-1/2 min-w-0">
                         <h1 className="text-4xl pb-10 text-white text-[clamp(1.25rem,2.5vw,2.5rem)]">[ ACCESS PERSONNEL FILE ]</h1>
                         <a href="/CGResume.pdf" target="_blank" rel="noopener noreferrer" className="block text-3xl pl-10 hover:text-cyan-400 hover:text-[2rem] cursor-pointer text-white text-[clamp(1rem,2vw,1.875rem)]">
                             ↓ RESUME
